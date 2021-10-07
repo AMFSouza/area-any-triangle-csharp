cb61f9af18cb142f99b7e77f5681a18e8ddad93c
AMFSouza <amfdeveloper@gmail.com>
2021-10-06 22:03:42 -0300

    Triangle class created

---
+++ AreaAnyTriangle/src/AreaAnyTriangle/Entities/Triangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AreaAnyTriangle.Entities
+{
+    class Triangle
+    {
+        public double aSide { get; set; }
+        public double bSide { get; set; }
+        public double cSide { get; set; }
+
+        public Triangle()
+        {
+
+        }
+
+        public Triangle(double aSide, double bSide, double cSide)
+        {
+            this.aSide = aSide;
+            this.bSide = bSide;
+            this.cSide = cSide;
+        }
+        public double Area()
+        {
+            double p = Factor();
+            return Math.Sqrt(p * (p - aSide) * (p - bSide) * (p - cSide));
+        }
+
+        private double Factor()
+        {
+            return (aSide + bSide + cSide) / 3;
+        }
+        public override string ToString()
+        {
+            return $"Area = {Area().ToString("F4", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
